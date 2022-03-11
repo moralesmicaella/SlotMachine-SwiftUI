00377d79d343cccb771272e91a7b99703f8d39a1
Micaella Morales <morales.micaella@gmail.com>
2022-03-10 20:59:18 -0800

    Initial Commit

---
+++ Slot Machine/App/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Micaella Morales on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
