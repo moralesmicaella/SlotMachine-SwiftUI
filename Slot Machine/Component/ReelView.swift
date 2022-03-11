6e332829d90bd91edfea1a31663a5dec34e9aefa
Micaella Morales <morales.micaella@gmail.com>
2022-03-10 22:03:23 -0800

    Add the reels and create an image modifier

---
+++ Slot Machine/Component/ReelView.swift
@@ -0,0 +1,23 @@
+//
+//  ReelView.swift
+//  Slot Machine
+//
+//  Created by Micaella Morales on 3/10/22.
+//
+
+import SwiftUI
+
+struct ReelView: View {
+  var body: some View {
+    Image(reel)
+      .resizable()
+      .modifier(ImageModifier())
+  }
+}
+
+struct ReelView_Previews: PreviewProvider {
+  static var previews: some View {
+    ReelView()
+      .previewLayout(.fixed(width: 220, height: 220))
+  }
+}
--- Slot Machine/View/ContentView.swift
+++ Slot Machine/View/ContentView.swift
@@ -54,6 +54,51 @@ struct ContentView: View {
         }
         
         // MARK: - SLOT MACHINE
+        VStack(alignment: .center, spacing: 0) {
+          // MARK: - REEL #1
+          ZStack {
+            ReelView()
+            
+            Image(bell)
+              .resizable()
+              .modifier(ImageModifier())
+          }
+          
+          HStack(alignment: .center, spacing: 0) {
+            // MARK: - REEL #2
+            ZStack {
+              ReelView()
+              
+              Image(seven)
+                .resizable()
+                .modifier(ImageModifier())
+            }
+            
+            Spacer()
+            
+            // MARK: - REEL #3
+            ZStack {
+              ReelView()
+              
+              Image(cherry)
+                .resizable()
+                .modifier(ImageModifier())
+            }
+          }
+          .frame(maxWidth: 500)
+          
+          // MARK: - SPIN BUTTON
+          Button {
+            print("Spin the reels")
+          } label: {
+            Image(spin)
+              .renderingMode(.original)
+              .resizable()
+              .modifier(ImageModifier())
+          }
+
+        }
+        .layoutPriority(2)
         
         // MARK: - FOOTER
         
